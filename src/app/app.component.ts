9c4b7dc69c49f5ac257686d5f6c46ce2dff14cf5
mmullis07 <eddiemull311@gmail.com>
2020-11-24 15:38:48 -0800

    Simple photo gallery app that allows you access the platforms native camera, take a photo, save it, display the saved photos, delete an individual photo. It also works natively on IOS and Android devices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,20 @@
-import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Component } from "@angular/core";
+import { Plugins } from "@capacitor/core";
+import { Platform } from "@ionic/angular";
+const { SplashScreen } = Plugins;
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+  constructor() {
     this.initializeApp();
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
+    SplashScreen.hide();
   }
 }
